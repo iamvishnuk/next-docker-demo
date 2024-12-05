2004229f19b7a9d3cd89ad316ab8fbde242bb212
vishnuk1609 <vishnu@glasier.in>
2024-12-05 15:55:31 +0530

    chore: lint on commitmsg

---
+++ commitlint.config.ts
@@ -0,0 +1,18 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'type']
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-max-length': [2, 'always', 72]
+  }
+};
+
+export default Configuration;
